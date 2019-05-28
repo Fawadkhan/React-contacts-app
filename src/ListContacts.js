b3e2e36f51d86e726c421b838eab865e5ecbd74a
Fawad khan <fovadkhan@outlook.com>
2019-05-29 02:04:18 +0500

    controlled components

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,26 +1,55 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-const ListContacts = props => {
-  return (
-    <ol className="contact-list">
-      {props.contacts.map(contact => (
-        <li key={contact.id} className="contact-list-item">
-          <div
-            className="contact-avatar"
-            style={{ background: `url(${contact.avatarURL})` }}
+class ListContacts extends Component {
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDelete: PropTypes.func.isRequired
+  };
+
+  state = {
+    query: ""
+  };
+
+  updateQuery = query => {
+    this.setState(() => ({
+      query: query.trim()
+    }));
+  };
+  render() {
+    return (
+      <div className="list-contacts">
+        <div className="top-contacts-top">
+          {JSON.stringify(this.state)}
+          <input
+            className="Search-contacts"
+            type="text"
+            placeholder="your text here"
+            value={this.state.query}
+            onChange={event => this.updateQuery(event.target.value)}
           />
-          <div className="contact-details">
-            <p>{contact.name}</p>
-            <p>{contact.handle}</p>
-          </div>
-          <button
-            className="contact-remove"
-            onClick={() => props.onDelete(contact)}
-          />
-        </li>
-      ))}
-    </ol>
-  );
-};
+        </div>
+        <ol className="contact-list">
+          {this.props.contacts.map(contact => (
+            <li key={contact.id} className="contact-list-item">
+              <div
+                className="contact-avatar"
+                style={{ background: `url(${contact.avatarURL})` }}
+              />
+              <div className="contact-details">
+                <p>{contact.name}</p>
+                <p>{contact.handle}</p>
+              </div>
+              <button
+                className="contact-remove"
+                onClick={() => this.props.onDelete(contact)}
+              />
+            </li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
+}
 
 export default ListContacts;
